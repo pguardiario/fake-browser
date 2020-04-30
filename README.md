6832b00eeadff250aec4ad9b13c615b9100cb86f
P Guardiario <p.guardiario@gmail.com>
2020-04-30 10:24:36 +0800

    add proxy support

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Make http2 requests that look like the real thing
 
 ```
 const f = require('fake-browser')
-let {headers, data} = await f.get('https://httpbin.org/ip'))
-let {headers, data} = await f.post('https://httpbin.org/post', 'foo=bar'))
-let {headers, data} = await f.post('https://httpbin.org/post', JSON.stringify({"foo": "bar"}), {json: true})
+let {headers, data} = await f.get('https://httpbin.org/ip')
+let {headers, data} = await f.get('https://httpbin.org/ip', { proxy: localhost:8888 })
+let {headers, data} = await f.post('https://httpbin.org/post', 'foo=bar')
+
 ```
+
+# Goals:
+- h2, https and http
+- proxy support for all 3
+- look exactly like real chrome requests
+- fast
+- zero dependencies
--- index.js
+++ index.js
@@ -1,172 +1,246 @@
-const http2 = require('http2')
-const https = require('https')
 const http = require('http')
+const https = require('https')
+const http2 = require('http2')
+const tls = require('tls')
 const zlib = require('zlib')
 const { PassThrough } = require('stream')
 
-class FakeBrowser{
-  constructor(ua = chrome, options = {}){
-    this.baseHeaders = {
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Accept-Language": "en-US;q=0.9,en;q=0.8",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Upgrade-Insecure-Requests": "1",
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+
+const baseHeaders = {
+  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "en-US;q=0.9,en;q=0.8",
+  "Sec-Fetch-Dest": "document",
+  "Sec-Fetch-Mode": "navigate",
+  "Sec-Fetch-Site": "none",
+  "Sec-Fetch-User": "?1",
+  "Upgrade-Insecure-Requests": "1",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+}
+
+const waitForData = async (res, socket) => {
+  return await new Promise((resolve, reject) => {
+    let stream = new PassThrough()
+    switch(res.headers["content-encoding"]){
+      case 'gzip':
+        res.pipe(zlib.createGunzip()).pipe(stream)
+        break
+      case 'deflate':
+        res.pipe(zlib.Inflate()).pipe(stream)
+        break
+      case 'br':
+        res.pipe(zlib.BrotliDecompress()).pipe(stream)
+        break
+      default:
+        res.pipe(stream)
+        break
     }
-    this._supported = {}
-  }
 
-  // keep track of http2 supported hosts
-  async supported(host) {
-    let supported = this._supported[host]
-    if(supported !== undefined) return supported
-    supported = await this.http2Support( host )
-    // console.log(`support for ${host}: ${!!supported}`)
-    this._supported[host] = supported
-    return supported
-  }
+    // req.setEncoding('utf8');
+    let buffers = []
 
-  async http2Support( host ) {
-    return await new Promise((resolve, reject) => {
-      const client = http2.connect(host)
-      client.on('error', (err) => {
-        resolve(false)
-      })
-      client.on('remoteSettings', (settings) => {
-        resolve(settings)
-      })
+    stream.on('data', (chunk) => {
+      buffers.push(chunk)
     })
-  }
 
-  async httpRequest(options){
-    const url = new URL(options.url)
-    return await new Promise((resolve, reject) => {
+    stream.on('error', (chunk) => {
+      buffers.push(chunk)
+    })
 
-    const o = {
-      hostname: url.hostname,
-      port: url.port || (options.https ? 443 : 80),
-      path: url.pathname + url.search,
-      method: options.method,
-      headers: {...this.baseHeaders, ...(options.headers || {})}
+    stream.on('end', () => {
+      // client.close()
+      resolve({headers: res.headers, data: Buffer.concat(buffers)})
+    })
+  })
+}
+
+const withHttp1 = async (url, options, socket) => {
+  let response = await new Promise(async (resolve, reject) => {
+    let parts = new URL(url)
+    let lib = parts.protocol === 'http:' ? http : https
+    // debugger
+    let connectOpts = socket ? {
+      createConnection: () => socket
+    } : {
+      host: parts.hostname,
+      port: portFor(url)
     }
-
-    const req = (options.https ? https : http).request(o, (res) => {
-      let data = ''
-      res.on('data', (chunk) => {
-        data += chunk
-      });
-
-      res.on('end', () => {
-        resolve({headers:res.headers, data})
-      });
-    });
-
+    connectOpts.path = parts.pathname
+    connectOpts.method = options.method
+    connectOpts.timeout = 10000
+    const req = lib.request(connectOpts, async (res) => {
+      // resolve(res.headers)
+      resolve(await waitForData(res, req))
+    })
     req.on('error', (e) => {
-      console.error(`problem with request: ${e.message}`);
-    });
-
-    // Write data to request body
-    // req.write(postData);
-    req.end();
-
+      return reject(e)
     })
+    req.setHeader('Host', parts.hostname)
+    let headers = {...baseHeaders, ...(options.headers || {})}
+    for(let key in headers){
+      req.setHeader(key, headers[key])
+    }
+    req.setHeader('Cookie', options.cookies || [])
+    if(options.method === 'POST'){
+      req.write(options.body)
+    }
+    req.end()
+  }).catch(e => {
+    // throw e
+  })
+  return response
+}
+
+const withHttp2 = async (url, options, socket) => {
+  return await new Promise(async (resolve, reject) => {
+    let parts = new URL(url)
+    const client = http2.connect(
+      {
+        host: parts.hostname,
+        port: portFor(url)
+      },
+      {
+        createConnection: () => socket,
+        timeout: 10000,
+      }
+    )
+    let headers = {...baseHeaders, ...(options.headers || {})}
+    const req = client.request({
+      ':path': parts.pathname,
+      ':method': options.method,
+      ...headers,
+      cookie: options.cookies || []
+    })
+    if(options.method === 'POST'){
+      req.write(options.body)
+    }
+    req.on('response', async (headers) => {
+      req.headers = headers
+      let data = await waitForData(req)
+      client.close()
+      resolve(data)
+    })
+    req.on('error', (e) => {
+      return reject(e)
+    })
+    req.end()
+  }).catch(e => {
+
+  })
+}
+
+const portFor = url => {
+  let parts = new URL(url)
+  if(parts.port) return parseInt(parts.port)
+  if(parts.protocol === 'https:') return 443
+  return 80
+}
+
+const socketFor = async (url, proxy) => {
+  if(!proxy.match(/:\/\//)) proxy = 'http://' + proxy
+  let socket = await new Promise ((resolve, reject) => {
+    let urlParts = new URL(url)
+    let proxyParts = new URL(proxy)
+
+    let options = {
+      host: proxyParts.hostname,
+      port: proxyParts.port || 80,
+      method: 'CONNECT',
+      timeout: 10000,
+      path: `${urlParts.hostname}:${portFor(url)}`, //
+    }
+    let req = http.request(options)
+
+    req.once('connect', function (res, socket, head) {
+      if(res.statusCode === 200){
+        if(!socket){
+          debugger
+        }
+        resolve(socket)
+      } else {
+        socket.destroy()
+        reject()
+      }
+    })
+    req.once('error', function (e) {
+      reject(e)
+    })
+
+    req.end()
+  }).catch(e => {
+    // debugger
+  })
+  return socket
+}
+
+const request = async (url, options = {}) => {
+  let socket
+  let response = await new Promise(async (resolve, reject) => {
+    let proxy
+    let parts = new URL(url)
+    let ALPNProtocols = ['h2', 'http/1.1']
+    if(options.proxy) {
+      proxy = options.proxy
+      socket = await socketFor(url, proxy)
+      if(!socket) return reject('bad proxy')
+      delete options.proxy
+    } else {
+
+    }
+
+    if(parts.protocol === 'http:'){
+      resolve(await withHttp1(url, options, socket))
+    } else {
+      const connectOpts = proxy ? {
+        socket,
+        ALPNProtocols
+      } : {
+        host: parts.hostname,
+        servername: parts.hostname,
+        port: portFor(url),
+        ALPNProtocols
+      }
+      const tlsConnection = tls.connect(connectOpts)
+      tlsConnection.once('error', (e) => {
+        reject(e)
+      })
+      tlsConnection.once('secureConnect', async () => {
+        if (tlsConnection.alpnProtocol === 'h2'){
+          resolve(await withHttp2(url, options, tlsConnection))
+        } else {
+          resolve(await withHttp1(url, options, tlsConnection))
+        }
+      })
+    }
+  }).catch(e => {
+    // console.log(e)
+  })
+
+  if(socket) socket.destroy()
+
+  if(response.headers[':status'] === 301 || response.headers[':status'] === 302){
+    let location = new URL(response.headers['location'], options.url).href
+    let newArgs = {...options, url: location}
+    return await this.request(url, newArgs)
   }
 
-  async request(options){
-    const url = new URL(options.url)
-    if(url.protocol === 'http:' || options.http || options.https) return await this.httpRequest(options)
-    if(!(await this.supported(url.protocol + '//' + url.host))) return await this.httpRequest(options)
-    let response = await new Promise((resolve, reject) => {
-      const client = http2.connect(url.protocol + '//' + url.host, {
-        // ca: fs.readFileSync('localhost-cert.pem')
-      })
+  return response
+}
 
-      client.on('error', (err) => {
-        reject(err)
-      });
-
-      const args = {
-        ':path': url.pathname + url.search,
-        ':method': options.method,
-        ...this.baseHeaders,
-        'cookie': options.cookies || []
-
-      }
-
-      if(options.method === 'POST'){
-        args["Content-Type"] = options.json ? "application/json" : "application/x-www-form-urlencoded"
-      }
-
-      const req = client.request( args )
-
-      req.on('response', (headers, flags) => {
-
-        let stream = new PassThrough()
-
-        switch(headers["content-encoding"]){
-          case 'gzip':
-            req.pipe(zlib.createGunzip()).pipe(stream)
-            break
-          case 'deflate':
-            req.pipe(zlib.Inflate()).pipe(stream)
-            break
-          case 'br':
-            req.pipe(zlib.BrotliDecompress()).pipe(stream)
-            break
-          default:
-            req.pipe(stream)
-            break
-        }
-
-        // req.setEncoding('utf8');
-        let buffers = []
-
-        stream.on('data', (chunk) => {
-          buffers.push(chunk)
-        })
-
-        stream.on('end', () => {
-          client.close()
-          resolve({headers, data: Buffer.concat(buffers)})
-        })
-
-      })
-
-      if(options.method === 'POST'){
-        req.write(options.body)
-      }
-      req.on('error', (err) => {
-        reject(err)
-      });
-
-      req.end();
-
-    }).catch(e => {
-      debugger
-    })
-    if(response.headers[':status'] === 301){
-      let location = new URL(response.headers['location'], options.url).href
-      let newArgs = {...options, url: location}
-      return await this.request(newArgs)
-    }
-    return response
+class FakeBrowser{
+  constructor(){
   }
 
   async get(url, options = {}){
-    return await this.request({
-      url,
+    return await request(url, {
       method: 'GET',
       ...options
     })
   }
 
   async post(url, body, options = {}){
-    return await this.request({
-      url,
+    return await request(url, {
       body,
       method: 'POST',
       ...options
@@ -174,16 +248,4 @@ class FakeBrowser{
   }
 }
 
-module.exports = new FakeBrowser('chrome', {})
-
-// ; (async() => {
-//   let f = new FakeBrowser('chrome', {})
-//   // let response = await f.get("https://www.physioboard.org.nz/search-register")
-//   let response = await f.get('https://www.amazon.com/Jutland-Unfinished-Personal-History-Controversy-ebook/dp/B01LXCAJJ1/ref=pd_sim_351_1/133-9215984-5997746?_encoding=UTF8&pd_rd_i=B01LXCAJJ1&pd_rd_r=a1cf8e48-c656-4a27-b38c-aa1f94a606dd&pd_rd_w=hiqGi&pd_rd_wg=JqyeP&pf_rd_p=9fec2710-b93d-4b3e-b3ca-e55dc1c5909a&pf_rd_r=ERR411HN72ZVPAD1G1YR&psc=1&refRID=ERR411HN72ZVPAD1G1YR')
-//   console.log('' + response.data)
-//   // let response = await f.get("https://www.physioboard.org.nz/search-register", {https: true})
-//   // let response = await f.get("https://www.themoviedb.org/tv/60694-last-week-tonight-with-john-oliver?language=en-US")
-//   // require('fs').writeFileSync('x.jpg', response.data)
-//   // let {headers, data} = await f.post('https://www.amazon.com/', JSON.stringify({"foo": "bar"}), {json: true})
-//   // debugger
-// })()
+module.exports = new FakeBrowser()
